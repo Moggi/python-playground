513773a067f274dc65452121d84ef865704fd82a
Paulo Moggi <paulohpmoggi@gmail.com>
2017-04-17 15:30:31 -0300

    test connection

---
+++ test_connection.py
@@ -0,0 +1,19 @@
+# http://stackoverflow.com/a/33117579
+import socket
+
+
+def test_internet_connection(host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+
+print(test_internet_connection())
