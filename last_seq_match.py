10898996084320e615d5d423f634641c6cee968b
Paulo Moggi <paulohpmoggi@gmail.com>
2017-06-30 15:30:01 -0300

    add a line finder script

---
+++ last_seq_match.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Go line by line of file1 and try to match with file2
+# returns the last match.
+# obs: file1 and file2 are sequencial
+
+import sys
+import re
+
+last_match = None
+
+
+if len(sys.argv) != 3:
+    print('usage: %s <file1> <file2>' % sys.argv[0])
+    print('It will return the last line of file1 that matches some line in file2')
+    exit(1)
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+
+with open(file2) as f:
+    src = f.read()
+    for line in open(file1):
+        line = line.strip()
+        if(line in src):
+            last_match = line
+        else:
+            break
+
+print( last_match if last_match else 'No matches' )
