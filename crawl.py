b8d82ffe69f5dffb92170c66685be5d2108fc2e3
Paulo Moggi <paulohpmoggi@gmail.com>
2018-11-23 16:49:55 -0200

    naive crawler

--- crawl.py
+++ crawl.py
@@ -1,7 +1,9 @@
 # From Udacity: Intro to Computer Science
 import requests
+from pprint import pprint
 
 url = 'http://docs.python-requests.org/en/master/'
+url = 'https://udacity.github.io/cs101x/index.html'
 
 
 def get_page(url):
@@ -19,16 +21,30 @@ def get_next_url(page):
     return url, end_quote
 
 
-def print_all_links(page):
+def get_all_links(page):
+    links = []
     while True:
         url, end_pos = get_next_url(page)
         if url:
-            print(url)
+            links.append(url)
             page = page[end_pos:]
         else:
             break
+    return links
 
 page = get_page(url)
 page_content = page.text if page.status_code == 200 else ''
 
-print_all_links(page_content)
+links = get_all_links(page_content)
+visited_links = []
+
+while len(links):
+    link = links.pop()
+    if link in visited_links:
+        continue
+    page = get_page(link)
+    visited_links.append(link)
+    page_content = page.text if page.status_code == 200 else ''
+    links += get_all_links(page_content)
+
+pprint(visited_links)
